78bf6163780490100b6d5a370fa2d6be55c0b875
umesh <umeshpurohit1975@gamil.com>
2021-05-15 23:55:12 +0530

    initial commit

---
+++ main/views.py
@@ -0,0 +1,25 @@
+from django.conf.urls import url
+from django.http.response import HttpResponse
+from django.shortcuts import render
+import json
+import urllib.request
+
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        if city:
+            res = urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=ab2e79c1529dff9433f9dbe2e310cc1e").read()
+            json_data = json.loads(res)
+            data = {
+                "country_code": str(json_data['sys']['country']),
+                "coordinates": str(json_data['coord']['lon']) + ' Longitude and ' + str(json_data['coord']['lat']) + ' Latitude',
+                "temp": str(json_data['main']['temp']),
+                "pressure": str(json_data['main']['pressure']),
+                "humidity": str(json_data['main']['humidity']),
+            }
+            return render(request, 'index.html', {'data':data, 'city':city})
+        else:
+            data = ''
+            return render(request, 'index.html', {'data':data})
+    else:
+        return render(request, 'index.html', {})
