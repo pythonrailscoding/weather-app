30d712b870c9b2dc1acb367ef6aebdcbaeeb6577
umesh <umeshpurohit1975@gamil.com>
2021-05-16 10:58:53 +0530

    updated

--- main/views.py
+++ main/views.py
@@ -11,12 +11,16 @@ def index(request):
             res = urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=ab2e79c1529dff9433f9dbe2e310cc1e").read()
             json_data = json.loads(res)
             data = {
+                "city_name": str(json_data["name"]),
                 "country_code": str(json_data['sys']['country']),
                 "coordinates": str(json_data['coord']['lon']) + ' Longitude and ' + str(json_data['coord']['lat']) + ' Latitude',
                 "temp": str(json_data['main']['temp']),
                 "pressure": str(json_data['main']['pressure']),
                 "humidity": str(json_data['main']['humidity']),
-            }
+                "description": json_data["weather"][0]["main"],
+                "icon": "http://openweathermap.org/img/w/"+json_data["weather"][0]["icon"]+".png",
+                }
+            
             return render(request, 'index.html', {'data':data, 'city':city})
         else:
             data = ''
